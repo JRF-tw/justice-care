7aea9acfa46c47ce1fd2fa3942e742e9f2e1bc07
billy3321 <billy3321@gmail.com>
2016-04-27 18:03:58 +0800

    fix seed and order

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -5,15 +5,15 @@ class Problem < ActiveRecord::Base
 
   scope :hot_order, -> {
     select("problems.*, (count(case when votes.created_at > '#{2.weeks.ago}' then 1 else null end) + problems.add) AS votes_count").
-    joins('LEFT OUTER JOIN "votes" ON "votes"."problem_id" = "problems"."id"').group("problems.id").order("votes_count DESC")
+    joins('LEFT OUTER JOIN "votes" ON "votes"."problem_id" = "problems"."id"').group("problems.id").order("votes_count DESC").order(id: :asc)
   }
 
   scope :lastest_order, -> {
-    order(created_at: :desc)
+    order(created_at: :desc).order(id: :asc)
   }
 
   scope :votes_order, -> {
-    joins('LEFT OUTER JOIN "votes" ON "votes"."problem_id" = "problems"."id"').group("problems.id").order("(count(votes.id) + problems.add) DESC")
+    joins('LEFT OUTER JOIN "votes" ON "votes"."problem_id" = "problems"."id"').group("problems.id").order("(count(votes.id) + problems.add) DESC").order(id: :asc)
   }
 
   def is_voted?(user)
