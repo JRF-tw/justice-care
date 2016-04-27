e954184f14830292962df0a152128671eba6414a
billy3321 <billy3321@gmail.com>
2016-04-27 18:16:31 +0800

    fix the after sign_in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
   protected
 
   def configure_devise_permitted_parameters
