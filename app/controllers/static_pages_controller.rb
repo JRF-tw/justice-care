441a88b9f10a293d0eb8cb80d8385a26024602d7
billy3321 <billy3321@gmail.com>
2016-05-12 12:49:01 +0800

    add keywords

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     set_meta_tags({
       title: '活動說明',
       description: '若對台灣司法懷有期許和熱情，現在，就是你站出來的時候。跟我們一起做「全民司改運動」！',
-      keywords: '司法改革',
+      keywords: '司法改革,全民司法改革運動,全民司改運動,民間司改會',
       og: {
         type: 'website',
         title: '活動說明',
@@ -24,7 +24,7 @@ class StaticPagesController < ApplicationController
     set_meta_tags({
       title: '健康檢查',
       description: '你心中重要的問題是什麼？來投票吧！',
-      keywords: '司法改革',
+      keywords: '司法改革,全民司法改革運動,全民司改運動,民間司改會',
       og: {
         type: 'website',
         title: '健康檢查',
@@ -37,7 +37,7 @@ class StaticPagesController < ApplicationController
     set_meta_tags({
       title: '評估體能',
       description: '評估問題背後的成因',
-      keywords: '司法改革',
+      keywords: '司法改革,全民司法改革運動,全民司改運動,民間司改會',
       og: {
         type: 'website',
         title: '評估體能',
@@ -50,7 +50,7 @@ class StaticPagesController < ApplicationController
     set_meta_tags({
       title: '訓練項目',
       description: '對問題生成的體制因素討論解方',
-      keywords: '司法改革',
+      keywords: '司法改革,全民司法改革運動,全民司改運動,民間司改會',
       og: {
         type: 'website',
         title: '訓練項目',
